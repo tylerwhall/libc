b8d0414435410383d3349c97e2772ae8150f498a
Tyler Hall <tylerwhall@gmail.com>
2024-10-09 02:00:59 -0400

    zephyr: remove fixed_width_ints for Rust 1.71
    I'm not sure why the duplicate definition wasn't a problem before.

--- src/zephyr/mod.rs
+++ src/zephyr/mod.rs
@@ -3,15 +3,6 @@
 //! More functions and definitions can be found in the more specific modules
 //! according to the platform in question.
 
-pub type int8_t = i8;
-pub type int16_t = i16;
-pub type int32_t = i32;
-pub type int64_t = i64;
-pub type uint8_t = u8;
-pub type uint16_t = u16;
-pub type uint32_t = u32;
-pub type uint64_t = u64;
-
 #[cfg(target_arch = "aarch64")]
 pub type c_char = u8;
 #[cfg(not(target_arch = "aarch64"))]
